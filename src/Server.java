f1a105ba5a3221bb5e5f7670249ad3b2c05ae1ef
Cj <cchiass2@oswego.edu>
2020-02-08 13:59:49 -0500

    4th  commit

--- src/Server.java
+++ src/Server.java
@@ -5,26 +5,43 @@ import java.net.Socket;
 public class Server {
     static final int PORT = 2770;// This is MY PORT up to 2779
     static String host = "pi.cs.oswego.edu";
+    DataInputStream in       =  null;
 
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(PORT);
-
+            System.out.println("Server started");
+            System.out.println("Waiting for a client ...");
 
             Socket s=serverSocket.accept();
-
-            DataInputStream dis=new DataInputStream(s.getInputStream());
-
+            System.out.println("Client accepted");
 
 
+            DataInputStream in = new DataInputStream(
+                    new BufferedInputStream(s.getInputStream()));
 
+            String line = "";
+            while (!line.equals("Over"))
+            {
+                try
+                {
+                    line = in.readUTF();
+                    System.out.println(line);
 
+                }
+                catch(IOException i)
+                {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection");
 
-
-        } catch (IOException e) {
+            // close connection
+            s.close();
+            in.close();
+        }catch (IOException e) {
             e.printStackTrace();
         }
-
 
     }
 }
